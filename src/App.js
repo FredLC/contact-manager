7d066d141d63b16dea838851b9a140e5a8d3ca68
fredlc <fredlefevre@outlook.com>
2019-05-19 10:38:32 -0400

    Added props to Contact component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Contact />
+      <Contact name="John Doe" email="johndoe@gmail.com" phone="555-555-5555" />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Contact extends Component {
   render() {
+    const { name, email, phone } = this.props;
     return (
       <div>
-        <h4>John Doe</h4>
+        <h4>{name}</h4>
         <ul>
-          <li>Email: johndoe@gmail.com</li>
-          <li>Phone: 555-555-5555</li>
+          <li>Email: {email}</li>
+          <li>Phone: {phone}</li>
         </ul>
       </div>
     );
   }
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
+};
