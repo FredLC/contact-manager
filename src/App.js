1619e6048035d1a21ca6e67df97962dc93acc85a
fredlc <fredlefevre@outlook.com>
2019-05-19 10:54:59 -0400

    Installed bootstrap and implemented basic style for homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "./components/Header";
 import Contact from "./components/Contact";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Contact name="John Doe" email="johndoe@gmail.com" phone="555-555-5555" />
+      <div className="container">
+        <Contact
+          name="John Doe"
+          email="johndoe@gmail.com"
+          phone="555-555-5555"
+        />
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,22 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Contact Manager</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          Contact Manager
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
