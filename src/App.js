1f1bf3d6a0837caec8a3268551547e496b83f765
fredlc <fredlefevre@outlook.com>
2019-05-29 12:59:26 -0400

    Added showContactInfo event handler to Contact component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
 import Contacts from "./components/Contacts";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
