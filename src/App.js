64fb584d25cf94d1239c31c632c78c845cf65c95
fredlc <fredlefevre@outlook.com>
2019-05-29 17:59:14 -0400

    Created AddContact component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/layout/Header";
+import AddContact from "./components/contacts/AddContact";
 import Contacts from "./components/contacts/Contacts";
 import { Provider } from "./context";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Header />
         <div className="container">
+          <AddContact />
           <Contacts />
         </div>
       </div>
---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+
+export default class AddContact extends Component {
+  state = {
+    name: "",
+    email: "",
+    phone: ""
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    const { name, email, phone } = this.state;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact</div>
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter name..."
+                className="form-control"
+                value={name}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter email..."
+                className="form-control"
+                value={email}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                placeholder="Enter phone number..."
+                className="form-control"
+                value={phone}
+                onChange={this.onChange}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-dark btn-block"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
