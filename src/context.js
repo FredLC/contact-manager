0ce1d702bacd79afcc3635d03d56604fe5b30198
fredlc <fredlefevre@outlook.com>
2019-05-29 14:02:19 -0400

    Refactored app to manage state using the context API

---
+++ src/context.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@gmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+        email: "janedoe@gmail.com",
+        phone: "777-777-7777"
+      },
+      {
+        id: 3,
+        name: "Joe Marambi",
+        email: "joemarambi@gmail.com",
+        phone: "819-918-8199"
+      }
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
