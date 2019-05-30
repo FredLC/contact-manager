8797084bb0de4eda4a6eddd99ae3d47dc0a1833d
fredlc <fredlefevre@outlook.com>
2019-05-30 12:02:32 -0400

    Implemented error checking on form submission

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,7 +7,8 @@ export default class AddContact extends Component {
   state = {
     name: "",
     email: "",
-    phone: ""
+    phone: "",
+    errors: {}
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -16,6 +17,21 @@ export default class AddContact extends Component {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
+
+    if (name === "") {
+      this.setState({ errors: { name: "Name is required" } });
+      return;
+    }
+
+    if (email === "") {
+      this.setState({ errors: { email: "Email is required" } });
+      return;
+    }
+
+    if (phone === "") {
+      this.setState({ errors: { phone: "Phone is required" } });
+      return;
+    }
 
     const newContact = {
       id: uuid(),
@@ -29,12 +45,13 @@ export default class AddContact extends Component {
     this.setState({
       name: "",
       email: "",
-      phone: ""
+      phone: "",
+      errors: {}
     });
   };
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -51,6 +68,7 @@ export default class AddContact extends Component {
                     placeholder="Enter Name"
                     value={name}
                     onChange={this.onChange}
+                    error={errors.name}
                   />
                   <TextInputGroup
                     label="Email"
@@ -59,6 +77,7 @@ export default class AddContact extends Component {
                     type="email"
                     value={email}
                     onChange={this.onChange}
+                    error={errors.email}
                   />
                   <TextInputGroup
                     label="Phone"
@@ -66,6 +85,7 @@ export default class AddContact extends Component {
                     placeholder="Enter Phone"
                     value={phone}
                     onChange={this.onChange}
+                    error={errors.phone}
                   />
                   <input
                     type="submit"
